39ba42bdafbf0df258164a8f2d73e004d684f63f
Deb Rajbanshi <106230397+Aurora0087@users.noreply.github.com>
2022-08-10 11:24:14 +0530

    Update link_list.java

--- link_list.java
+++ link_list.java
@@ -3,12 +3,12 @@ package ds.ex1.link_list;
 import java.util.Objects;
 import java.util.Scanner;
 
-class node{
+class node {
     int value;
     node p;
-    public node(int value){
-        this.value=value;
-        this.p=null;
+    public node(int value) {
+        this.value = value;
+        this.p = null;
     }
 }
 
@@ -27,7 +27,6 @@ public class link_list {
     }
     public static void display(){
         node dis=head;
-        int size=0;
         if (dis==null){
             System.out.println("\nList is empty.");
             return;
@@ -36,22 +35,72 @@ public class link_list {
         while (dis!=null){
             System.out.print(dis.value+" | ");
             dis=dis.p;
-            size++;
         }
-        System.out.println("\nSize of this list = "+size);
     }
+    public static int insert(int data,int index){
+        node dis=head;
+        int size=0;
+        if (dis==null){
+            System.out.println("\n:: List is empty ::");
+            return 0;
+        } else if (index==1) {
+            node new_node=new node(data);
+            new_node.p=head;
+            head=new_node;
+            return ++size;
+        }
+        for (int i=0;i<index-2;i++){
+            dis=dis.p;
+        }
+        node new_node=new node(data);
+        new_node.p=dis.p;
+        dis.p=new_node;
+        return ++size;
+    }
+
+    public static int delete(int index){
+        int size=0;
+        node dis=head;
+        for (int i=0;i<index-2;i++){
+            dis=dis.p;
+        }
+        dis.p=dis.p.p;
+        return ++size;
+    }
+
     public static void main(String[] args) {
-        int data,i=1;
+        int data;
         String ex;
         Scanner s=new Scanner(System.in);
         System.out.println("\n\t:: link list ::");
-        do {
-            System.out.print("*Enter element number "+(i++)+" = ");
+        System.out.println(":: How many element you want to print in this link list ::");
+        int size=s.nextInt();
+        for (int i=0;i<size;i++){
+            System.out.print("*Enter element number "+(i+1)+" = ");
             data=s.nextInt();
             insert(data);
-            System.out.print("Want to continue Y/N :: ");
-            ex=s.next();
-        }while (!Objects.equals(ex, "n") && !Objects.equals(ex, "N"));
+        }
         display();
+        System.out.println("\n:: Enter which element you want to add in list ::");
+        data=s.nextInt();
+        System.out.println("\n:: Which index you want to add this element ::");
+        int index=s.nextInt();
+        if (index<1 ||index>size+1){
+            System.out.println("\n:: Not capable of storing this element in this index ::");
+        }
+        else {
+        size+=insert(data,index);
+        display();
+        }
+        System.out.println("\n:: Which index's element you want to delete from list ::");
+        index=s.nextInt();
+        if (index<1 ||index>size+1){
+            System.out.println("\n:: Not capable of deleting this element in this index ::");
+        }
+        else {
+            size-=delete(index);
+            display();
+        }
+        
     }
 }
